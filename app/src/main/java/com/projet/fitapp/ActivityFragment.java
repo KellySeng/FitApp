87860a1896c9bfcb6d892a2385dc09af2399a5a3
Skelly98 <tsukiamekun@gmail.com>
2020-02-03 04:01:03 +0100

    update

--- app/src/main/java/com/projet/fitapp/ActivityFragment.java
+++ app/src/main/java/com/projet/fitapp/ActivityFragment.java
@@ -1,24 +1,138 @@
 package com.projet.fitapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.text.DateFormat;
+import java.util.concurrent.TimeUnit;
+
 
 public class ActivityFragment extends Fragment {
 
 
+    Handler handler;
+    static final int REQUEST_OAUTH = 3;
+    String TAG = "HistoryFrag";
+    TextView logger;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_activity, container, false);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if (msg.what == 0) {
+                    Bundle stuff = msg.getData();
+                    logthis(stuff.getString("logthis"));
+                }
+                return true;
+            }
+        });
+        logger = (TextView) myView.findViewById(R.id.log);
 
 
-
-        return inflater.inflate(R.layout.fragment_activity,container,false);
+        return myView;
     }
+
+
+    public void logthis(String item) {
+        Log.i(TAG, item);
+        logger.append(item + "\n");
+    }
+
+    private boolean hasOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        return GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(getContext()), fitnessOptions);
+    }
+
+    /**
+     * Launches the Google SignIn activity to request OAuth permission for the user.
+     */
+    private void requestOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        GoogleSignIn.requestPermissions(
+                this,
+                REQUEST_OAUTH,
+                GoogleSignIn.getLastSignedInAccount(getContext()),
+                fitnessOptions);
+    }
+
+
+    private FitnessOptions getFitnessSignInOptions() {
+        return FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+                .build();
+    }
+
+    private Task<DataSet> displayTodayData() {
+        Log.i(TAG, "Reading History API results for today of Steps");
+        return Fitness.getHistoryClient(getActivity(), GoogleSignIn.getLastSignedInAccount(getContext()))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        Log.i(TAG, "Reading History API results for today");
+                        showDataSet(dataSet);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Failed to read DailyTotal for Steps.");
+                    }
+                });
+
+    }
+
+    private void showDataSet(DataSet dataSet) {
+        sendmessage("Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        DateFormat timeFormat = DateFormat.getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) { sendmessage("Data point:");
+            sendmessage("\tType: " + dp.getDataType().getName());
+            sendmessage("\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            sendmessage("\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                sendmessage("\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
+
+    public void sendmessage(String logthis) {
+        Bundle b = new Bundle();
+        b.putString("logthis", logthis);
+        Message msg = handler.obtainMessage();
+        msg.setData(b);
+        msg.arg1 = 1;
+        msg.what = 0;
+        handler.sendMessage(msg);
+
+    }
+
 }
--- app/src/main/java/com/projet/fitapp/MainActivity.java
+++ app/src/main/java/com/projet/fitapp/MainActivity.java
@@ -19,6 +19,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
